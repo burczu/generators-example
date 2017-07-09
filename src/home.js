e2f7ad4e006e5b454b99a10f475f269a8acb7417
burczu <b.dybowski@gmail.com>
2017-07-09 19:05:13 +0200

    next and yield examples

--- src/home.js
+++ src/home.js
@@ -1,12 +1,28 @@
 import 'babel-polyfill';
 
+// function* decorator() {
+//   console.log('dec');
+//   yield 'test';
+//   console.log('orator');
+//   return 'end!';
+// }
+//
+// const iterator = decorator();
+//
+// const nextRes1 = iterator.next();
+// const nextRes2 = iterator.next();
+//
+// console.log(nextRes1);
+// console.log(nextRes2);
+
 function* decorator() {
-  console.log('dec');
-  yield;
-  console.log('orator');
+  for (let i = 0; i < 10; i++) {
+    yield i;
+  }
 }
 
 const iterator = decorator();
 
-iterator.next();
-iterator.next();
+for (const value of iterator) {
+  console.log(value);
+}
